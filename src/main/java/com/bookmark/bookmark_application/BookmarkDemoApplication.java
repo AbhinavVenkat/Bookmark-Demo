876c10d4c3b2de711f401cc4f138d2720caff799
Abhinavlv <abhinav.venkat@anywhere.co>
2025-01-07 17:38:12 +0530

    setting up file system

---
+++ src/main/java/com/bookmark/bookmark_application/BookmarkDemoApplication.java
@@ -0,0 +1,21 @@
+package com.bookmark;
+
+
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+
+public class BookmarkDemoApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BookmarkDemoApplication.class, args);
+
+    }
+
+}
